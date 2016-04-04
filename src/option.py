e3ff532c89115f285378afb5a2fc0173d3a893d0
Tyler Brough <Tyler Brough>
2016-04-04 15:54:50 -0600

    Added directories, and option.py source file.

---
+++ src/option.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class VanillaOption(object):
+    def __init__(self, strike, expiry):
+        self.strike = strike
+        self.expiry = expiry
+        
+    def payoff(self):
+        pass
+    
+
