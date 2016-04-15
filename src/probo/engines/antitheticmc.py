09dcf8c0560014eb2bf168823f4578e509e39af0
Stew Higham <stew.higham@gmail.com>
2016-04-14 23:52:46 -0600

    Update antitheticmc.py

--- src/probo/engines/antitheticmc.py
+++ src/probo/engines/antitheticmc.py
@@ -6,22 +6,29 @@ class NaiveMC(PricingEngine):
 
     def __init__(self, nreps):
         self.__nreps = nreps
-
+        
+#  The Standard Monte Carlo        
     def calculate(self, option):
         rate, spot, sigma, div = option.marketData
         disc = np.exp(rate * option.expiry)
         dt = option.expiry
-        #z = np.random.normal(size=self.__nreps)
-        for i in range(nreps):
-            
-
+        z = np.random.normal(size=self.__nreps)
         nudt = (rate - 0.5 * sigma * sigma) * dt
         sidt = sigma * np.sqrt(dt)
-
+        
         St = np.zeros((self.__nreps, ))
         Ct = np.zeros((self.__nreps, ))
         St = spot * np.exp(nudt + sidt * z)
         Ct = option.payoff(St)
         price = disc * Ct.mean()
-
+        
+# Antithetic Path
+        St_antithetic = np.zeros((self.__nreps, ))
+        Ct_antithetic = np.zeros((self.__nreps, ))
+        St_antithetic = spot * np.exp(nudt - sidt * z)
+        Ct_antithetic = option.payoff(St_antithetic)
+        price_antithetic = disc * Ct_antithetic.mean()
+        
+        
         return price
+        return price_antithetic
