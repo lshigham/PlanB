2d93f16c6eb5e60142fba724db57d71d6437273c
Stew Higham <stew.higham@gmail.com>
2016-04-15 00:08:45 -0600

    Update option.py

--- src/option.py
+++ src/option.py
@@ -8,4 +8,12 @@ class VanillaOption(object):
     def payoff(self):
         pass
     
+class Exotic_option(object):
+    def __init__(self, strike, expiry):
+        self.strike = strike
+        self.expiry = expiry
+        
+    def payoff(self):
+        pass
+    
 
